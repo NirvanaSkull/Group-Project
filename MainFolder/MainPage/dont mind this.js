d899fc541b6dfe3c266b1f54b84694cce3db0b7e
NirvanaSkull <jwang419@ucsc.edu>
2022-05-19 10:31:31 -0700

    Create dont mind this.js

---
+++ MainFolder/MainPage/dont mind this.js
@@ -0,0 +1,95 @@
+var buttonEl = document.getElementById('1');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("www.nationalgeographic.org/encyclopedia/wetland/");
+});
+
+var buttonEl = document.getElementById('2');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("ipm.ucanr.edu/PMG/WEEDS/willowherbs.html");
+});
+
+var buttonEl = document.getElementById('3');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('4');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('5');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('6');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('7');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('8');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('9');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('10');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('11');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('12');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('13');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('14');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('15');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
+
+var buttonEl = document.getElementById('16');
+buttonEl.addEventListener('click', function() {
+	alert("You Been Redirected.");
+  window.location.replace("https://www.youtube.com/channel/UCltrzXl9ZVwZEtjMBuyPpNg");
+});
